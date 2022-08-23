d71c5e9f1f9d6f30af4f9aaa8a26cd353f979f89
Oxana22 <nesterova.slt@gmail.com>
2022-08-23 13:37:22 +0300

    H

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите целое число, a = ");
+int a = Convert.ToInt32( Console.ReadLine()  );
+ 
+Console.Write("Введите целое число, b = ");
+int b = Convert.ToInt32( Console.ReadLine()  );
+
+Console.Write("Введите целое число, c = ");
+int c = Convert.ToInt32( Console.ReadLine()  );
+
+int max=a;
+
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+
+Console.Write("max= ");
+Console.WriteLine(max);
+
+
+
+
+
+
+// Console.Write("Введите целое число, a = ");
+// int a = Convert.ToInt32( Console.ReadLine()  );
+// Console.Write("Введите целое число отличное от числа (a), b = ");
+// int b = Convert.ToInt32( Console.ReadLine()  );
+
+// if (a>b)
+// {
+//     Console.WriteLine("max = " + a );
+// }
+// else
+// {
+//     Console.WriteLine("max = " + b );
+// }
