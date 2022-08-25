b51e8296b8678342ed188b4d1135cd7ccc2a091d
Oxana22 <nesterova.slt@gmail.com>
2022-08-25 22:59:25 +0300

    Ex1

---
+++ LessonEx1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+void PrintNumbers(int num1, int num2)
+{
+int x = new Random().Next(10,100); 
+for (int i=num1; i<num2+1; i++) Console.Write($"{i}, ");
+// for (int i=num1; i<num2+1;i++) Console.Write(i+", ");
+Console.WriteLine();
+}
+
+PrintNumbers(2,8);
+PrintNumbers(1,7);
+PrintNumbers(6,12);
